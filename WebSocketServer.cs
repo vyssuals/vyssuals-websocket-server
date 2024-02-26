ff6835b37488891cc35e71fbee8d8b84bff5955a
Yskert Schindel <yskert.schindel@gmail.com>
2024-02-26 14:22:39 +0100

    changed to class lib

--- WebSocketServer.cs
+++ WebSocketServer.cs
@@ -6,63 +6,72 @@ using System.Threading.Tasks;
 using System.Text;
 using System.Collections.Generic;
 
-public class WebSocketServer
+namespace Vyssuals.WsServer
 {
-    private List<WebSocket> clients = new List<WebSocket>();
-
-    public async Task RunServerAsync()
+    public class WebSocketServer
     {
-        var httpListener = new HttpListener();
-        httpListener.Prefixes.Add("http://localhost:8184/");
-        httpListener.Start();
-        Console.WriteLine("Server started.");
+        private List<WebSocket> clients = new List<WebSocket>();
 
-        while (true)
+        public async Task RunServerAsync(string url)
         {
-            var context = await httpListener.GetContextAsync();
+            var httpListener = new HttpListener();
+            httpListener.Prefixes.Add(url);
+            httpListener.Start();
+            Console.WriteLine($"Server started");
 
-            if (context.Request.IsWebSocketRequest)
+            while (true)
             {
-                var webSocketContext = await context.AcceptWebSocketAsync(null);
-                var webSocket = webSocketContext.WebSocket;
+                var context = await httpListener.GetContextAsync();
 
-                clients.Add(webSocket);
-
-                _ = HandleIncomingMessagesAsync(webSocket);
-            }
-            else
-            {
-                context.Response.StatusCode = 400;
-                context.Response.Close();
-            }
-        }
-    }
-
-    private async Task HandleIncomingMessagesAsync(WebSocket sender)
-    {
-        var buffer = new byte[1024];
-
-        while (sender.State == WebSocketState.Open)
-        {
-            var result = await sender.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
-
-            if (result.MessageType == WebSocketMessageType.Text)
-            {
-                var message = Encoding.UTF8.GetString(buffer, 0, result.Count);
-                Console.WriteLine($"Received message from client: {message}");
-
-                foreach (var client in clients)
+                if (context.Request.IsWebSocketRequest)
                 {
-                    if (client != sender && client.State == WebSocketState.Open)
-                    {
-                        await client.SendAsync(new ArraySegment<byte>(buffer, 0, result.Count), WebSocketMessageType.Text, result.EndOfMessage, CancellationToken.None);
-                    }
+                    var webSocketContext = await context.AcceptWebSocketAsync(null);
+                    var webSocket = webSocketContext.WebSocket;
+
+                    clients.Add(webSocket);
+
+                    _ = HandleIncomingMessagesAsync(webSocket);
+
+                    Console.WriteLine($"Client connected. Total clients: {clients.Count}");
+                }
+                else
+                {
+                    context.Response.StatusCode = 400;
+                    context.Response.Close();
+
+                    Console.WriteLine($"Bad request. Total clients: {clients.Count}");
                 }
             }
-            else if (result.MessageType == WebSocketMessageType.Close)
+        }
+
+        private async Task HandleIncomingMessagesAsync(WebSocket sender)
+        {
+            var buffer = new byte[1024];
+
+            while (sender.State == WebSocketState.Open)
             {
-                await sender.CloseAsync(WebSocketCloseStatus.NormalClosure, "", CancellationToken.None);
-                clients.Remove(sender);
+                var result = await sender.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+
+                if (result.MessageType == WebSocketMessageType.Text)
+                {
+                    var message = Encoding.UTF8.GetString(buffer, 0, result.Count);
+                    Console.WriteLine($"Received message from client: {message}");
+
+                    foreach (var client in clients)
+                    {
+                        if (client != sender && client.State == WebSocketState.Open)
+                        {
+                            await client.SendAsync(new ArraySegment<byte>(buffer, 0, result.Count), WebSocketMessageType.Text, result.EndOfMessage, CancellationToken.None);
+                        }
+                    }
+                }
+                else if (result.MessageType == WebSocketMessageType.Close)
+                {
+                    await sender.CloseAsync(WebSocketCloseStatus.NormalClosure, "", CancellationToken.None);
+                    clients.Remove(sender);
+
+                    Console.WriteLine($"Client disconnected. Total clients: {clients.Count}");
+                }
             }
         }
     }
