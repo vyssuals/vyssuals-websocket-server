f850c34f7c14a51baa8364ce3bd2163ebf748b62
Yskert Schindel <yskert.schindel@gmail.com>
2024-03-26 17:06:35 +0100

    Update WebSocketServer.cs

--- WebSocketServer.cs
+++ WebSocketServer.cs
@@ -93,7 +93,7 @@ namespace Vyssuals.WsServer
                     if (result.MessageType == WebSocketMessageType.Text)
                     {
                         var message = Encoding.UTF8.GetString(buffer, 0, result.Count);
-                        Debug.WriteLine($"wsServer: Received message from client: {message}");
+                        Debug.WriteLine($"wsServer: Received message from client: {sender}");
 
                         foreach (var client in clients)
                         {
